04a27cea535a23dc89a52acf0081b1a65c0422b1
gxeni <xenidisxenidis@gmail.com>
2026-09-01 13:34:01 +0300

    Initial commit

---
+++ e16095-assignment/src/main/java/com/unipi/e16095_assignment/E16095AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.unipi.e16095_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E16095AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E16095AssignmentApplication.class, args);
+	}
+
+}
---
+++ e16095-assignment/src/test/java/com/unipi/e16095_assignment/E16095AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.e16095_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E16095AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
