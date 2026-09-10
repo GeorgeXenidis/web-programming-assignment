cce83475f5c2a4fd3aafc60cbd519c4e61e98f26
gxeni <xenidisxenidis@gmail.com>
2026-09-10 19:07:24 +0300

    Fixes on login rest calls and userRole handling

--- e16095-assignment/src/main/java/com/unipi/e16095_assignment/controllers/LoginController.java
+++ e16095-assignment/src/main/java/com/unipi/e16095_assignment/controllers/LoginController.java
@@ -2,14 +2,10 @@ package com.unipi.e16095_assignment.controllers;
 
 import com.unipi.e16095_assignment.dtos.LoginRequestDto;
 import com.unipi.e16095_assignment.dtos.LoginResponseDto;
-import com.unipi.e16095_assignment.enums.RoleEnum;
 import com.unipi.e16095_assignment.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,19 +21,10 @@ public class LoginController {
     }
 
     @PostMapping({"", "/"})
-    public ModelAndView performLoginAction(@RequestParam LoginRequestDto loginRequestDto) {
+    public ModelAndView performLoginAction(@ModelAttribute LoginRequestDto loginRequestDto) {
         LoginResponseDto loginResponseDto = loginService.performLoginAction(loginRequestDto);
 
-        switch (loginResponseDto.getRoleEnum().toString()) {
-            case "ADMIN":
-                return new ModelAndView("adminMainPage");
-            case "TECHNICIAN":
-                return new ModelAndView("technicianMainPage");
-            case "SIMPLE_USER":
-                return new ModelAndView("simpleUserMainPage");
-            default:
-                return new ModelAndView("roleNotFoundErrorPage");
-        }
+        return loginService.constructModelForResponse(loginResponseDto);
     }
 
 }
--- e16095-assignment/src/main/java/com/unipi/e16095_assignment/services/LoginService.java
+++ e16095-assignment/src/main/java/com/unipi/e16095_assignment/services/LoginService.java
@@ -1,11 +1,13 @@
 package com.unipi.e16095_assignment.services;
 
-import com.unipi.e16095_assignment.UserRepository;
+import com.unipi.e16095_assignment.enums.RoleEnum;
+import com.unipi.e16095_assignment.repositories.UserRepository;
 import com.unipi.e16095_assignment.dtos.LoginRequestDto;
 import com.unipi.e16095_assignment.dtos.LoginResponseDto;
 import com.unipi.e16095_assignment.entities.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
@@ -18,19 +20,46 @@ public class LoginService {
     private UserRepository userRepository;
 
     public LoginResponseDto performLoginAction(LoginRequestDto loginRequestDto) {
-        LoginResponseDto loginResponseDto;
-
         Optional<Users> optionalUser = userRepository.findByUsername(loginRequestDto.getUsername());
         if (optionalUser.isEmpty()) {
-            return loginResponseDto = new LoginResponseDto(null, null, null, null, false);
+            return new LoginResponseDto(null, null, null, null, false);
         }
 
         Users user = optionalUser.get();
         if(!user.getPassword().equals(loginRequestDto.getPassword())) {
-            return loginResponseDto = new LoginResponseDto(null, loginRequestDto.getUsername(), null, null, false);
+            return new LoginResponseDto(null, loginRequestDto.getUsername(), null, null, false);
         }
 
         return userEntityToResponseDtoMapper(user);
+    }
+
+    public ModelAndView constructModelForResponse(LoginResponseDto loginResponseDto) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        RoleEnum userRole = loginResponseDto.getRoleEnum();
+        if(userRole == null) {
+            modelAndView.setViewName("errorPage");
+            modelAndView.addObject("errorMessage", "User role not found...");
+
+            return modelAndView;
+        }
+        switch (userRole) {
+            case ADMIN -> {
+                modelAndView.setViewName("adminMainPage");
+            }
+            case TECHNICIAN -> {
+                modelAndView.setViewName("technicianMainPage");
+            }
+            case SIMPLE_USER -> {
+                modelAndView.setViewName("simpleUserMainPage");
+            }
+            default -> {
+                modelAndView.setViewName("errorPage");
+                modelAndView.addObject("errorMessage", "Could not resolve a page to return...");
+            }
+        }
+
+        return modelAndView;
     }
 
 }
