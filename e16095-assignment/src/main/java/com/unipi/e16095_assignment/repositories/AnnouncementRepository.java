3c5e90c3e6420f3e4f8ee61350dd4b7632713b7b
gxeni <xenidisxenidis@gmail.com>
2026-09-15 11:41:45 +0300

    Annotate AnnouncementRepository with @Repository

--- e16095-assignment/src/main/java/com/unipi/e16095_assignment/repositories/AnnouncementRepository.java
+++ e16095-assignment/src/main/java/com/unipi/e16095_assignment/repositories/AnnouncementRepository.java
@@ -2,6 +2,8 @@ package com.unipi.e16095_assignment.repositories;
 
 import com.unipi.e16095_assignment.entities.Announcements;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnnouncementRepository extends JpaRepository<Announcements, Long> {
 }
